1b89771a4160ca6fbcf783b89c2c234cb0436b6d
Bhuvanesh Singla <bhuvanesh.221ai014@nitk.edu.in>
2025-08-19 22:44:27 +0530

    new args added

--- run.bat
+++ run.bat
@@ -1,13 +1,13 @@
 @echo off
 python video_proctor.py ^
-    --face Datasets\front_test.avi ^
-    --hand Datasets\side_test.avi ^
+    --face Datasets\nocheat\front_test.avi ^
+    --hand Datasets\nocheat\side_test.avi ^
     --target Datasets\ID.png ^
-    --lstm-model Models\temporal_proctor_trained_on_processed.pt ^
-    --static-model Models\lightgbm_cheating_model_20250801_200619.pkl ^
-    --static-scaler Models\scaler_20250801_200619.pkl ^
-    --static-metadata Models\model_metadata_20250801_200619.pkl ^
-    --test-duration 5 ^
-    --mediapipe-task Models\face_landmarker.task ^
-    --yolo-model Models\OEP_YOLOv11n.pt ^
+    --lstm-model best_models\temporal_proctor_trained_on_processed.pt ^
+    --static-model best_models\lightgbm_cheating_model_20250818_132555.pkl ^
+    --static-scaler best_models\scaler_20250818_132555.pkl ^
+    --static-metadata best_models\model_metadata_20250818_132555.pkl ^
+    --mediapipe-task best_models\face_landmarker.task ^
+    --yolo-model best_models\OEP_YOLOv11n.pt ^
+    --process-fps 1 ^
     --debug
